42266b80607549d3e6b539c84841d37830b5ff99
lwhitelaw <48133276+lwhitelaw@users.noreply.github.com>
2023-08-24 22:35:38 -0400

    runner

--- java/src/me/lwhitelaw/lwrand/Main.java
+++ java/src/me/lwhitelaw/lwrand/Main.java
@@ -1,10 +1,25 @@
 package me.lwhitelaw.lwrand;
 
 public class Main {
-
+	private static final int BUF_SIZE = 1048576;
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		if (args.length > 0 && args[0].equals("lwrand32")) {
+			LWRand32 r = new LWRand32();
+			byte[] b = new byte[BUF_SIZE];
+			for (;;) {
+				r.nextBytes(b);
+				System.out.writeBytes(b);
+			}
+		}
+		if (args.length > 0 && args[0].equals("lwrand64")) {
+			LWRand64 r = new LWRand64();
+			byte[] b = new byte[BUF_SIZE];
+			for (;;) {
+				r.nextBytes(b);
+				System.out.writeBytes(b);
+			}
+		}
+		System.out.println("Usage: (lwrand32 | lwrand64)");
 	}
-
 }
